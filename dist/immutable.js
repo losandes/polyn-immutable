370818f2d4c7af4f4b1b3b28f517b596e85c29ee
&y <andes.collab@gmail.com>
2019-08-27 10:10:21 -0400

    Upgrades Dependencies (#12)
    Upgrades all packages, and addresses [GHSA-3gx7-xhv7-5mx3](https://github.com/mysticatea/eslint-utils/security/advisories/GHSA-3gx7-xhv7-5mx3)

--- dist/immutable.js
+++ dist/immutable.js
@@ -26,7 +26,9 @@ function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.
 
 function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } }
 
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(source, true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(source).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
@@ -149,7 +151,7 @@ function _defineProperty(obj, key, value) { if (key in obj) { Object.definePrope
 
         var _removeFunctions$opti = _objectSpread({}, {
           removeFunctions: false
-        }, options),
+        }, {}, options),
             removeFunctions = _removeFunctions$opti.removeFunctions;
 
         Object.keys(shallowClone).forEach(function (key) {
@@ -228,7 +230,7 @@ function _defineProperty(obj, key, value) { if (key in obj) { Object.definePrope
       function PolynImmutable(config) {
         config = _objectSpread({}, {
           Validator: Validator
-        }, config);
+        }, {}, config);
         /**
          * Creates a Validator (@polyn/blueprint by default) and returns a
          * function for creating new instances of objects that get validated
@@ -244,7 +246,7 @@ function _defineProperty(obj, key, value) { if (key in obj) { Object.definePrope
 
           var _functionsOnPrototype = _objectSpread({}, {
             functionsOnPrototype: false
-          }, options),
+          }, {}, options),
               functionsOnPrototype = _functionsOnPrototype.functionsOnPrototype; // NOTE the classes, and freezeArray are in here, so their
           // prototypes don't cross-contaminate
 
